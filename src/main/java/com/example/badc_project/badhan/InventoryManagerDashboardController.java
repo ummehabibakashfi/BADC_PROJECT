45d57906274cfade168f805dff39cc161652c190
badhanrahman <farzinabadhan377@gmail.com>
2025-08-12 13:21:42 +0600

    inventory manager dashboard added

---
+++ src/main/java/com/example/badc_project/badhan/InventoryManagerDashboardController.java
@@ -0,0 +1,78 @@
+package com.example.badc_project.badhan;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class InventoryManagerDashboardController
+{
+    @javafx.fxml.FXML
+    private Button equipmentReturnButton;
+    @javafx.fxml.FXML
+    private Button previousButton;
+    @javafx.fxml.FXML
+    private Button manageDistributionButton;
+    @javafx.fxml.FXML
+    private Button inventoryReportsButton;
+    @javafx.fxml.FXML
+    private Button logoutButton;
+    @javafx.fxml.FXML
+    private Label inventoryManagerLabel;
+    @javafx.fxml.FXML
+    private Button inventoryLevelButton;
+    @javafx.fxml.FXML
+    private Button InventoryAuditsButton;
+    @javafx.fxml.FXML
+    private Button newSuppliesButton;
+    @javafx.fxml.FXML
+    private Button equipmentAvailabilitybutton;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void equipmentAvailabilityOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void previoun(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateInventoryReportsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageDistributionOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutButtonOnAction(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void previousButtonOnAction(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleEquipmentReturnOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateInventoryRecordsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void conductInventoryAuditsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void newSuppliesOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void inventoryLevelOnAction(ActionEvent actionEvent) {
+    }
+}

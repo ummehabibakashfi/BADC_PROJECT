69b7c10b293255cdde8be488ee3696e958459918
badhanrahman <farzinabadhan377@gmail.com>
2025-08-12 13:41:00 +0600

    manage distribution added

---
+++ src/main/java/com/example/badc_project/badhan/ManageDistributionDashboardController.java
@@ -0,0 +1,24 @@
+package com.example.badc_project.badhan;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class ManageDistributionDashboardController
+{
+    @javafx.fxml.FXML
+    private TextField idTextField;
+    @javafx.fxml.FXML
+    private TextField productDetailsTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void updateButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void previousButtonOnAction(ActionEvent actionEvent) {
+    }
+}

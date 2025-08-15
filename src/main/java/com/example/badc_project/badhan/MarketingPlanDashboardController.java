bfc818a2d410c22c9edb65cc39dbd582e682cfc1
badhanrahman <farzinabadhan377@gmail.com>
2025-08-15 17:35:01 +0600

    Marketing Plan Dashboard Added

---
+++ src/main/java/com/example/badc_project/badhan/MarketingPlanDashboardController.java
@@ -0,0 +1,32 @@
+package com.example.badc_project.badhan;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class MarketingPlanDashboardController
+{
+    @javafx.fxml.FXML
+    private TextField budgetTextField;
+    @javafx.fxml.FXML
+    private TextField audienceTextField;
+    @javafx.fxml.FXML
+    private TextField timelineTextField;
+    @javafx.fxml.FXML
+    private TextField objectiveTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void launchButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void previousButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButtonOnAction(ActionEvent actionEvent) {
+    }
+}

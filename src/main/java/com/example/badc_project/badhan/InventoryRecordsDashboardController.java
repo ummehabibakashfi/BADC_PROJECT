d0ba0a65360de63b0a2f1a68424e71da0e4af5ad
badhanrahman <farzinabadhan377@gmail.com>
2025-08-15 08:22:16 +0600

    Inventory Records added

---
+++ src/main/java/com/example/badc_project/badhan/InventoryRecordsDashboardController.java
@@ -0,0 +1,35 @@
+package com.example.badc_project.badhan;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class InventoryRecordsDashboardController
+{
+    @javafx.fxml.FXML
+    private TextField idTextField;
+    @javafx.fxml.FXML
+    private TextField typeTextField;
+    @javafx.fxml.FXML
+    private TextField modelTextField;
+    @javafx.fxml.FXML
+    private TableView inventoryOutputTableview;
+    @javafx.fxml.FXML
+    private TextField quantityTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButtonOnAc(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateInventoryButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void previousButtonOnAction(ActionEvent actionEvent) {
+    }
+}

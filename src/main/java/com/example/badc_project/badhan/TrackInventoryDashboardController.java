577ddfe34a0b04e8ae2d8c40581e044989237d73
badhanrahman <farzinabadhan377@gmail.com>
2025-08-15 17:05:03 +0600

    Track Inventory Dashboard Added

--- src/main/java/com/example/badc_project/badhan/TrackInventoryDashboardController.java
+++ src/main/java/com/example/badc_project/badhan/TrackInventoryDashboardController.java
@@ -1,7 +1,37 @@
 package com.example.badc_project.badhan;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class TrackInventoryDashboardController
 {
     @javafx.fxml.FXML
+    private TextField idTextField;
+    @javafx.fxml.FXML
+    private TextField productTypeTextField;
+    @javafx.fxml.FXML
+    private TableView inventoryOutputTableView;
+    @javafx.fxml.FXML
+    private TextField quantityTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void displayButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void previousButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void getDataButtonOnAction(ActionEvent actionEvent) {
+    }
+}

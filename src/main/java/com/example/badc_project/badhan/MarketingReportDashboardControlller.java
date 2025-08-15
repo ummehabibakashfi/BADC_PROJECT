dab6b1739c131ab26d641a63fa09fff5a9e2bf89
badhanrahman <farzinabadhan377@gmail.com>
2025-08-15 17:58:34 +0600

    Marketing Report Dashboard Added

---
+++ src/main/java/com/example/badc_project/badhan/MarketingReportDashboardControlller.java
@@ -0,0 +1,30 @@
+package com.example.badc_project.badhan;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class MarketingReportDashboardControlller
+{
+    @javafx.fxml.FXML
+    private TextField idTextField;
+    @javafx.fxml.FXML
+    private TextField dataTextField;
+    @javafx.fxml.FXML
+    private TextField reportTypeTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void previousButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateButtonOnAction(ActionEvent actionEvent) {
+    }
+}

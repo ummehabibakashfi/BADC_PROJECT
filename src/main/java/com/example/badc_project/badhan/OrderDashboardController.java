01c751ca95f24147a4e7ce7c705d461e86e08836
badhanrahman <farzinabadhan377@gmail.com>
2025-08-15 07:37:15 +0600

    order supplies dashboard updated

---
+++ src/main/java/com/example/badc_project/badhan/OrderDashboardController.java
@@ -0,0 +1,30 @@
+package com.example.badc_project.badhan;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class OrderDashboardController
+{
+    @javafx.fxml.FXML
+    private TextField idTextField;
+    @javafx.fxml.FXML
+    private TextField dupplierTextField;
+    @javafx.fxml.FXML
+    private TextField productDetailsTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void placeOrderButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void previousButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButtonOnAction(ActionEvent actionEvent) {
+    }
+}

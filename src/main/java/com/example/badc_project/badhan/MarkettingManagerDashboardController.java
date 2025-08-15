e323f54ae24e0f0c3684de9129d4f7f59666bcae
badhanrahman <farzinabadhan377@gmail.com>
2025-08-15 17:24:41 +0600

    Marketing Manager Dashboard Added

---
+++ src/main/java/com/example/badc_project/badhan/MarkettingManagerDashboardController.java
@@ -0,0 +1,42 @@
+package com.example.badc_project.badhan;
+
+import javafx.event.ActionEvent;
+
+public class MarkettingManagerDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void measureCampaingSuccess(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void S(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createMarketingReportButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createMarketingPlanButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void investigateMarketPreferanceButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void launchPromotionalOfferButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void understandCustomerNeedsButtonOnAction(ActionEvent actionEvent) {
+    }
+}

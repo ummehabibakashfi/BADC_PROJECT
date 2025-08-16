8068fca97fb4e04de23bbf3f5b95ad888db778d8
badhanrahman <farzinabadhan377@gmail.com>
2025-08-16 06:18:42 +0600

    update Added

---
+++ src/main/java/com/example/badc_project/badhan/AvailabilityDashboardController.java
@@ -0,0 +1,32 @@
+package com.example.badc_project.badhan;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class AvailabilityDashboardController
+{
+    @javafx.fxml.FXML
+    private TextField idTextField;
+    @javafx.fxml.FXML
+    private TextField productDetailsTextField;
+    @javafx.fxml.FXML
+    private TextField categoryTextField;
+    @javafx.fxml.FXML
+    private TextField quantityTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void updateButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void previousButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButtonOnAction(ActionEvent actionEvent) {
+    }
+}
